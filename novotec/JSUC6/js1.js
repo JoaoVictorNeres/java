d43ea947d4ec69702e3f53db5dba6a85c1c367c1
JoaoVictorNeres <neres3903@gmail.com>
2023-08-22 11:32:15 -0300

    aula 2208

--- novotec/JSUC6/js1.js
+++ novotec/JSUC6/js1.js
@@ -1,4 +1,7 @@
 let soma = 5 +3;
 
-console.log(soma)
-
+console.log(soma);
+let soma2 = 5 + "3";
+console.log(soma2);
+let mult = 5 * "3";
+console.log(mult);
---
+++ novotec/JSUC6/js2.js
@@ -0,0 +1,5 @@
+let texto1 = "Ola";
+let texto2 = "mundo";
+var numero = 2;
+let mensagem = texto1 + " " + texto2 + " " + numero
+console.log(mensagem);
